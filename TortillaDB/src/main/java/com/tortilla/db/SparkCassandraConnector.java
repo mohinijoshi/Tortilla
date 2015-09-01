eb054667d3fd9bc5b87cc31a3d10e8df18ccd79a
mvsjoshi@hotmail.com <mani!!)%>
2015-09-01 10:57:38 -0700

    /service created

--- TortillaDB/src/main/java/com/tortilla/db/SparkCassandraConnector.java
+++ TortillaDB/src/main/java/com/tortilla/db/SparkCassandraConnector.java
@@ -1,5 +1,9 @@
 package com.tortilla.db;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.SparkContext;
+import org.apache.spark.sql.cassandra.CassandraSQLContext;
+
 /**
  * Created with IntelliJ IDEA.
  * User: uttam
@@ -8,4 +12,23 @@ package com.tortilla.db;
  * To change this template use File | Settings | File Templates.
  */
 public class SparkCassandraConnector {
+    public static SparkConf conf;
+    public static CassandraSQLContext sqlContext;
+
+    public SparkCassandraConnector() {
+        //following 2 lines connect to spark and cassandra from google cloud respectively.
+        conf = new SparkConf().setAppName("spark count").setMaster("local[2]")  ;
+        conf.set("spark.cassandra.connection.host", "146.148.48.21");
+        conf.set("spark.driver.allowMultipleContexts", String.valueOf(true));
+    }
+
+    public SparkConf GetSparkConfig() {
+        return conf;
+    }
+
+    public CassandraSQLContext GetCassandraSQLContext() {
+        SparkContext sc = new SparkContext(conf);
+        sqlContext = new CassandraSQLContext(sc);
+        return sqlContext;
+    }
 }
--- TortillaServices/src/main/java/com/Tortilla/Service/JSONService.java
+++ TortillaServices/src/main/java/com/Tortilla/Service/JSONService.java
@@ -1,4 +1,10 @@
-package Service;
+package com.tortilla.Service;
+
+import com.tortilla.Service.Track;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * Created with IntelliJ IDEA.
@@ -7,5 +13,28 @@ package Service;
  * Time: 11:48 PM
  * To change this template use File | Settings | File Templates.
  */
+@Path("/json/metallica")
 public class JSONService {
+    @GET
+    @Path("/get")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Track getTrackInJSON() {
+
+        Track track = new Track();
+        track.setTitle("Enter Sandman");
+        track.setSinger("Metallica");
+
+        return track;
+
+    }
+
+    @POST
+    @Path("/post")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createTrackInJSON(Track track) {
+
+        String result = "Track saved : " + track;
+        return Response.status(201).entity(result).build();
+
+    }
 }
