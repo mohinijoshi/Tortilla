5c6c068f356cbb0984da1345685093b3783b553c
mvsjoshi@hotmail.com <mani!!)%>
2015-09-01 12:25:35 -0700

    Anjular js changes

--- TortillaServices/src/main/java/com/Tortilla/Service/JSONService.java
+++ TortillaServices/src/main/java/com/Tortilla/Service/JSONService.java
@@ -1,7 +1,5 @@
 package com.tortilla.Service;
 
-import com.tortilla.Service.Track;
-
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -15,17 +13,16 @@ import javax.ws.rs.core.Response;
  */
 @Path("/json/metallica")
 public class JSONService {
+    /*
     @GET
-    @Path("/get")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Track getTrackInJSON() {
+     @Path("/get")
+     @Produces(MediaType.APPLICATION_JSON)
+     public Track getTrackInJSON() {
 
         Track track = new Track();
         track.setTitle("Enter Sandman");
         track.setSinger("Metallica");
-
         return track;
-
     }
 
     @POST
@@ -34,6 +31,27 @@ public class JSONService {
     public Response createTrackInJSON(Track track) {
 
         String result = "Track saved : " + track;
+        return Response.status(201).entity(result).build();
+
+    }        */
+
+    @GET
+    @Path("/get")
+    @Produces(MediaType.APPLICATION_JSON)
+    public CallReport getCallReportInJSON() {
+
+        CallReport callReport = new CallReport();
+        callReport.setFirst_name("Mohini");
+        callReport.setLast_name("Joshi");
+        return callReport;
+    }
+
+    @POST
+    @Path("/post")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createCallReportInJSON(CallReport callReport) {
+
+        String result = "CallReport saved : " + callReport;
         return Response.status(201).entity(result).build();
 
     }
